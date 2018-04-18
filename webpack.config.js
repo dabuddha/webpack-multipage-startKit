bde158e644c8ee89b6b7f26e9bed085daca6f241
hao <dabuddha@126.com>
2018-04-18 17:13:48 +0800

    添加https模式

--- webpack.config.js
+++ webpack.config.js
@@ -40,152 +40,158 @@ function generateHtmlPlugins (templateDir) {
 
 const htmlPlugins = generateHtmlPlugins('./src/html/views')
 
-module.exports = {
-  entry: entries('./src/html/views'),
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: './js/[name].[chunkhash:8].js',
-    publicPath: '/'
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
-        use: [
-          {
-            loader: 'babel-loader'
-          },
-          {
-            loader: 'eslint-loader',
-            options: {
-              emitWarning: true,
-              formatter: require('eslint-friendly-formatter'),
-              configFile: '.eslintrc'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        loader: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [{
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
-          {
-            loader: 'postcss-loader'
-          }
-          ]
-        })
-      },
-      {
-        test: /\.(sass|scss)$/,
-        include: path.resolve(__dirname, 'src/scss'),
-        use: ExtractTextPlugin.extract({
+module.exports = (env, options) => {
+  const mode = options.env.mode
+  const https = options.https
+  process.env.mode = mode
+  return {
+    mode,
+    entry: entries('./src/html/views'),
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: './js/[name].[chunkhash:8].js',
+      publicPath: '/'
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: path.resolve(__dirname, 'src'),
           use: [
             {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true,
-                minimize: true,
-                url: false
-              }
+              loader: 'babel-loader'
             },
             {
-              loader: 'postcss-loader',
+              loader: 'eslint-loader',
               options: {
-                sourceMap: true
+                emitWarning: true,
+                formatter: require('eslint-friendly-formatter'),
+                configFile: '.eslintrc'
               }
-            },
-            {
-              loader: 'sass-loader'
             }
           ]
-        })
-      },
-      {
-        test: /\.art$/,
-        loader: 'art-template-loader',
-        options: {
-          // art-template options (if necessary)
-          // @see https://github.com/aui/art-template
-          htmlResourceRules: [/\bsrc="([^"]*)"/, /\bdata-src="([^"]*)"/, /\bdata-background-image="([^"]*)"/]
+        },
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          loader: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [{
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1
+              }
+            },
+            {
+              loader: 'postcss-loader'
+            }
+            ]
+          })
+        },
+        {
+          test: /\.(sass|scss)$/,
+          include: path.resolve(__dirname, 'src/scss'),
+          use: ExtractTextPlugin.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true,
+                  minimize: true,
+                  url: false
+                }
+              },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader'
+              }
+            ]
+          })
+        },
+        {
+          test: /\.art$/,
+          loader: 'art-template-loader',
+          options: {
+            // art-template options (if necessary)
+            // @see https://github.com/aui/art-template
+            htmlResourceRules: [/\bsrc="([^"]*)"/, /\bdata-src="([^"]*)"/, /\bdata-background-image="([^"]*)"/]
+          }
+        },
+        // 图片
+        {
+          test: /\.(png|jpg|gif)$/,
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            name: 'image/[name].[hash:8].[ext]'
+          }
+        },
+        // 媒体文件
+        {
+          test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: 'media/[name].[hash:7].[ext]'
+          }
         }
-      },
-      // 图片
-      {
-        test: /\.(png|jpg|gif)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 8192,
-          name: 'image/[name].[hash:8].[ext]'
+      ]
+    },
+    plugins: [
+      new webpack.NoEmitOnErrorsPlugin(),
+      new FriendlyErrorsWebpackPlugin({
+        compilationSuccessInfo: {
+          messages: [https ? `Your website is running here: https://localhost:8080` : `Your website is running here: http://localhost:8080`]
         }
-      },
-      // 媒体文件
-      {
-        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: 'media/[name].[hash:7].[ext]'
+      }),
+      new ExtractTextPlugin({
+        filename: './css/[name].[hash:8].css',
+        allChunks: true
+      }),
+      new OpenBrowserPlugin({
+        url: https ? `https://localhost:${PORT}/webpack-dev-server` : `http://localhost:${PORT}/webpack-dev-server`
+      }),
+      new CleanWebpackPlugin(['dist']),
+      new CopyWebpackPlugin([
+        // {
+        //   from: './src/fonts',
+        //   to: './fonts'
+        // },
+        {
+          from: './src/favicon',
+          to: './favicon'
         }
+        // {
+        //   from: './src/img',
+        //   to: './img'
+        // }
+      ]),
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        'window.$': 'jquery'
+      })
+    ].concat(htmlPlugins),
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      port: PORT,
+      quiet: true,
+      overlay: {
+        warnings: true,
+        errors: true
       }
-    ]
-  },
-  plugins: [
-    new webpack.NoEmitOnErrorsPlugin(),
-    new FriendlyErrorsWebpackPlugin({
-      compilationSuccessInfo: {
-        messages: [`Your website is running here: http://localhost:8080`]
-      }
-    }),
-    new ExtractTextPlugin({
-      filename: './css/[name].[hash:8].css',
-      allChunks: true
-    }),
-    new OpenBrowserPlugin({
-      url: `http://localhost:${PORT}/webpack-dev-server`
-    }),
-    new CleanWebpackPlugin(['dist']),
-    new CopyWebpackPlugin([
-      // {
-      //   from: './src/fonts',
-      //   to: './fonts'
-      // },
-      {
-        from: './src/favicon',
-        to: './favicon'
-      }
-      // {
-      //   from: './src/img',
-      //   to: './img'
-      // }
-    ]),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery',
-      'window.$': 'jquery'
-    })
-  ].concat(htmlPlugins),
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    port: PORT,
-    quiet: true,
-    overlay: {
-      warnings: true,
-      errors: true
     }
   }
 }
